e13b272a3a0d4f7b3d094c1a8786c0fbe4a6ec8b
kody0220 <giants.ks24@icloud.com>
2020-02-19 22:03:57 +0900

    create auto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,4 @@
 $(function(){ 
-  console.log(last_message_id);
   var buildHTML = function(message) {
     if (message.content && message.image) {
       //data-idが反映されるようにしている
@@ -54,41 +53,64 @@ $(function(){
     };
     return html;
   };
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- last_message_id = $('.message:last').data("message-id");
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
- .done(function(messages) {
-  if (messages.length !== 0) {
-    //追加するHTMLの入れ物を作る
-    var insertHTML = '';
-    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-    $.each(messages, function(i, message) {
-      insertHTML += buildHTML(message)
+ 
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('メッセージの送信に失敗しました')
+    })
+    .always(function(){
+      $('.form__submit').prop('disabled', false);
+    })
+  });
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id);
+
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
     });
-    //メッセージが入ったHTMLに、入れ物ごと追加
-    $('.messages').append(insertHTML);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-  }
-})
-  .fail(function(){
-    alert('メッセージの送信に失敗しました')
-  })
-  .always(function(){
-    $('.form__submit').prop('disabled', false);
-  })
-})
-  setInterval(reloadMessages, 7000);
+  };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
